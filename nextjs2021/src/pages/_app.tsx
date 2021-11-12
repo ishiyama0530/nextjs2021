061f699219a263394618b754416d746fc8a2c1d7
ishiyama <masanobu-ishiyama@gmo.jp>
2021-11-12 14:07:25 +0900

    cleanup husky and lint-staged

--- nextjs2021/src/pages/_app.tsx
+++ nextjs2021/src/pages/_app.tsx
@@ -2,13 +2,6 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-var a = 0
-var a = 0;
-var a = 0;
-var a = 0;
-var a = 0;
-var a = 0;
-var a = 0;
   return <Component {...pageProps} />
 }
 
