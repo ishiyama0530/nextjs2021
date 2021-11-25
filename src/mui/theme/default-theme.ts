bf9894c1b2d4e64dc238edf034ad0b03a3e76091
ishiyama <masanobu-ishiyama@gmo.jp>
2021-11-25 18:57:19 +0900

    change primary color

--- src/mui/theme/default-theme.ts
+++ src/mui/theme/default-theme.ts
@@ -1,6 +1,11 @@
 import { createTheme } from "@mui/material"
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#191970",
+    },
+  },
   components: {
     MuiIcon: {
       defaultProps: {
