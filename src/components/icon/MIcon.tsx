5e3c66b2c142191716c0e7af230bcf3055ca0ae5
ishiyama <masanobu-ishiyama@gmo.jp>
2021-11-25 18:53:36 +0900

    add size property

--- src/components/icon/MIcon.tsx
+++ src/components/icon/MIcon.tsx
@@ -4,16 +4,20 @@ export type Props = {
   children: React.ReactNode
   color?: string
   style?: React.CSSProperties | undefined
+  size?: "18px" | "24px" | "36px" | "48px"
 }
 
 export function MIcon(props: Readonly<Props>) {
-  const { children, color, style } = props
-  const s = { ...style }
+  const { children, color, style, size } = props
+  const cStyle = { ...style }
   if (color) {
-    s.color = color
+    cStyle.color = color
   }
+
+  cStyle.fontSize = size ?? "24px"
+
   return (
-    <span className="material-icons" style={s}>
+    <span className="material-icons" style={cStyle}>
       {children}
     </span>
   )
