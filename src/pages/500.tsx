433a9ebb9dff9d4bb3d82baa5c6d445fe78649d9
ishiyama <masanobu-ishiyama@gmo.jp>
2021-11-26 12:51:55 +0900

    cleanup import

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,5 +1,4 @@
-import { Typography } from "@mui/material"
-import { styled } from "@mui/system"
+import { Typography, styled } from "@mui/material"
 import React from "react"
 import { CentralBox } from "../components/box/CentralBox"
 
